2c1f8e004314b16e8ac4f24f1ac754a9c6bb2c24
Artut Fattakhov <86257431+ArchFa@users.noreply.github.com>
2022-07-20 17:24:05 +0500

    add

--- Untitled-1.py
+++ Untitled-1.py
@@ -5,6 +5,8 @@ import streamlit as st
 
 
 # %%
+st.title("Промокоды1")
+
 upload_file = "offers_responds_06_01_07_01.txt"
 
 df = pd.read_csv(upload_file, sep='|')
@@ -27,15 +29,28 @@ df = df.sort_values(by=['state', 'offer_id', 'respond_created_at'])
 df['sequence']=df.groupby('offer_id').cumcount() + 1
 
 # %%
+st.title("Промокоды2")
+
+
+# %%
 df = df.query('sequence == 1')
+
+# %%
+st.title("Промокоды3")
 
 # %%
 df['diff_time'] = df['respond_created_at'] - df['offer_created_at']
 
 # %%
+st.title("Промокоды4")
+
+# %%
 df['diff_time'] = df['diff_time'].astype('timedelta64[m]')
 df['diff_time'] = df['diff_time'] / 60
 df['diff_time'] = df['diff_time'].round(2)
+
+# %%
+st.title("Промокоды5")
 
 # %%
 def time_distribution(diff_time):
@@ -64,7 +79,13 @@ def time_distribution(diff_time):
 df['time_period'] = df['diff_time'].apply(time_distribution)
 
 # %%
+st.title("Промокоды6")
+
+# %%
 df = df.drop(['client_phone', 'offer_created_at', 'responds_id', 'offer_id','executor_phone', 'respond_created_at', 'sequence', 'diff_time'], axis=1)
+
+# %%
+st.title("Промокоды7")
 
 # %%
 import numpy as np
@@ -75,18 +96,33 @@ pivot_df
 pivot_df = pivot_df.reset_index()
 
 # %%
+st.title("Промокоды8")
+
+# %%
 df = df.sort_values(by=['state', 'category_id'])
+
+# %%
+st.title("Промокоды9")
 
 # %%
 df_l = pivot_df.pivot_table(index=['state', 'category_id'], columns='time_period')
 
 # %%
+st.title("Промокоды10")
+
+# %%
 df_l = df_l.reset_index()
+
+# %%
+st.title("Промокоды11")
 
 # %%
 df_l.columns = ['state', 'category_id', 'Более 72 часов', 'Менее 30 минут', 'От 1 до 2 часов',
                'От 10 до 15 часов', 'От 15 до 24 часов', 'От 2 до 5 часов', 'От 24 до 36 часов',
                'От 30 минут до 1 часа', 'От 36 до 48 часов', 'От 48 до 72 часов', 'От 5 до 10 часов']
+
+# %%
+st.title("Промокоды12")
 
 # %%
 new_df = df_l.reindex(columns=['state', 'category_id', 'Менее 30 минут', 'От 30 минут до 1 часа',
@@ -101,6 +137,9 @@ new_df = new_df.fillna(0)
 for col in new_df.columns:
     if str(new_df[col].dtype) == 'float64':
         new_df[col] = new_df[col].astype(int)
+
+# %%
+st.title("Промокоды13")
 
 # %%
 new_df['total_task'] = new_df[['Менее 30 минут', 'От 30 минут до 1 часа',
